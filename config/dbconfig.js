37af52bef1dbb90a71cdaa9e0d7f82eeffcc4b8c
Maytecalvert <166619703+JulietaWeber@users.noreply.github.com>
2025-10-31 09:58:30 -0300

    bruh

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: "postgres",
-  logging: false,
+  dialectModule: pg, 
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false } 
+  },
+  logging: false
 });
