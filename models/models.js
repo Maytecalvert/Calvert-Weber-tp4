84be3688387675fecc5597c582cc263c008a5c1e
Maytecalvert <166619703+JulietaWeber@users.noreply.github.com>
2025-10-31 08:52:14 -0300

    ...

--- models/models.js
+++ models/models.js
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from "sequelize";
-import { sequelize } from "../../config/dbconfig.js";
+import { sequelize } from "../config/dbconfig.js";
 
 export class Usuario extends Model {}
 Usuario.init({
